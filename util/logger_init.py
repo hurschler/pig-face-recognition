399e82b45c33dc21e1f2a321317d13ed815a93b0
avatar <avatar@cybnet.ch>
2021-03-11 18:49:29 +0100

    find log_config.yaml

--- util/logger_init.py
+++ util/logger_init.py
@@ -3,7 +3,10 @@ import yaml
 import os
 
 # Import and config logging
-with open(os.path.join('../log_config.yaml'), 'r') as f:
+main_base = os.path.dirname(__file__)
+log_config_file = os.path.join(main_base, 'log_config.yaml')
+
+with open(log_config_file, 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
 
