994d6ff6af49e10e81dc73b23419706d2e356649
avatar <avatar@cybnet.ch>
2021-03-11 11:19:42 +0100

    add Badges and .idea to gitignore

---
+++ app/server-pig-face-detection.py
@@ -0,0 +1,158 @@
+from flask import Flask, jsonify, request, render_template, redirect, abort, send_from_directory, Response
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_bootstrap import Bootstrap
+from src.api_spec import spec
+from PIL import Image as Pil_Image
+import marshmallow as ma
+from flask_smorest import Api, Blueprint, abort
+from flask import Flask
+from flask_apispec import use_kwargs, marshal_with
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
+
+import os
+import cv2
+import numpy as np
+import jsonpickle
+import base64
+import io
+
+SWAGGER_URL = '/api/docs'
+API_URL = "/api/swagger.json"
+
+app = Flask(__name__)
+
+IMAGE_UPLOADS = "./upload"
+app.config["IMAGE_UPLOADS"] = IMAGE_UPLOADS
+
+
+class ImageSchema(ma.Schema):
+    id = ma.fields.Int()
+    type = ma.fields.String()
+    image = ma.fields.String()
+
+
+@app.route("/api/swagger.json")
+def create_swagger_spec():
+    return jsonify(spec.to_dict())
+
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+    if request.method == "POST":
+        if request.files:
+            img = cv2.imdecode(np.fromstring(request.files["image"].read(), np.uint8), cv2.IMREAD_UNCHANGED)
+
+            image = request.files["image"]
+            print(image)
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            return redirect(request.url)
+    return render_template("upload_image.html")
+
+
+@app.route("/files")
+def list_files():
+    """Endpoint to list files on the server."""
+    files = []
+    for filename in os.listdir(IMAGE_UPLOADS):
+        path = os.path.join(IMAGE_UPLOADS, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return jsonify(files)
+
+
+@app.route("/files/<path:path>")
+def get_file(path):
+    """Download a file."""
+    return send_from_directory(IMAGE_UPLOADS, path, as_attachment=True)
+
+
+@app.route("/index.html")
+def main():
+    return render_template('index.html')
+
+
+@app.route("/files/<filename>", methods=["POST"])
+def post_file(filename):
+    """Upload a file."""
+
+    if "/" in filename:
+        # Return 400 BAD REQUEST
+        abort(400, "no subdirectories allowed")
+
+    with open(os.path.join("./upload", filename), "wb") as fp:
+        fp.write(request.data)
+
+    # Return 201 CREATED
+    return "", 201
+
+
+# route http posts to this method
+# @api.route('/api/test')
+@app.route('/api/postimage', methods=['POST'])
+def post_image():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # do some fancy processing here....
+    pil_img = Pil_Image.fromarray(im_rgb)
+    pil_img.save(os.path.join(app.config["IMAGE_UPLOADS"], "11.jpg"))
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+@use_kwargs(ImageSchema)
+@marshal_with(ImageSchema, code=201)
+@app.route('/api/postjsonimage', methods=['POST'])
+def post_json_image():
+    payload = request.form.to_dict(flat=False)
+    im_b64 = payload['image'][0]  # remember that now each key corresponds to list.
+    im_id = payload['id'][0]
+    im_binary = base64.b64decode(im_b64)
+    buf = io.BytesIO(im_binary)
+    pil_img = Pil_Image.open(buf)
+    pil_img.save(os.path.join(app.config["IMAGE_UPLOADS"], im_id))
+    open_cv_image = np.array(pil_img)
+    # Convert RGB to BGR
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
+
+    response = {'message': 'image received. size={}x{}'.format(open_cv_image.shape[1], open_cv_image.shape[0])}
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+@use_kwargs(ImageSchema)
+@marshal_with(ImageSchema)
+@app.route('/api/sendpet', methods=['POST'])
+def put(**kwargs):
+    print('pet erhalten')
+    print(**kwargs)
+    return Response(response='pet', status=200, mimetype="application/json")
+
+
+if __name__ == "__main__":
+    Bootstrap(app)
+
+    app.config.update({
+        'APISPEC_SPEC': APISpec(
+            title='pig_face_detection',
+            version='v1',
+            openapi_version='3.0.2',
+            plugins=[MarshmallowPlugin()],
+        ),
+        'APISPEC_SWAGGER_URL': '/swagger/',
+    })
+    docs = FlaskApiSpec(app)
+    docs.register(post_json_image)
+    docs.register(put)
+    # app.run(host='147.88.62.72', port=8080)
+    app.run("127.0.0.1", port=8080)
