5986a21245273f00028b5828fbc735854673ebab
avatar <avatar@cybnet.ch>
2021-03-20 17:23:16 +0100

    fix some tests (not found example image)

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -14,7 +14,7 @@ class TestPreprocessing(TestCase):
         print ('Image full path: ', os.path.join(config.image_sample_path, config.image_example_name))
         img = self.preprocessing.readImage(config.image_sample_path, config.image_example_name)
         if img is None:
-            img = self.preprocessing.readImage(os.path.join('/home/runner/work/pig-face-recognition/pig-face-recognition', config.image_example_name))
+            img = self.preprocessing.readImage(os.path.join('/home/runner/work/pig-face-recognition/pig-face-recognition/sample', config.image_example_name))
         self.assertIsNotNone(img,'Image is None')
         self.assertEqual(31.764733264481336, self.preprocessing.computeSharpness(img))
 
