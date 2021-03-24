dc38f528edb02ff49eeb596b90de8173d6c985e7
avatar <avatar@cybnet.ch>
2021-03-24 12:31:57 +0100

    add config files

---
+++ util/config.py
@@ -0,0 +1,20 @@
+import os
+
+
+image_root_dir_path = r'/Users/patrickrichner/Desktop/FH/OneDrive - Hochschule Luzern/BDA2021/07_Daten/bearbeitet_mit_label'
+
+image_train_dir_path = os.path.join(image_root_dir_path, "train")
+
+image_train_with_subdir_path = os.path.join(image_root_dir_path, "cropped-subdir-08-03-2021")
+
+image_example_name = r"DSC_V1_6460_2238.JPG"
+
+image_example_blue_name = r"0-DSC_V1_6494_2109.JPG-blue-background.jpg"
+
+build_server_path = r'/home/runner/work/pig-face-recognition/pig-face-recognition'
+
+output_path = r'/Users/patrickrichner/Desktop/FH/11.Semester/Bda2021/pig-face-recognition/output'
+
+image_sample_path = r'/Users/patrickrichner/Desktop/FH/11.Semester/Bda2021/pig-face-recognition/sample'
+
+max_image_number = 30
