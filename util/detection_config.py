73c05fa9da318db310ff5e0ae0fd8301889ddcc7
PatrickRichner <patrick.richner@stud.hslu.ch>
2021-03-25 10:43:58 +0100

    Change detection_config - Add config reader

--- util/detection_config.py
+++ util/detection_config.py
@@ -1,4 +1,27 @@
 import os
+import configparser
+
+local_property_file = '../local.properties'
+build_property_file = '/home/runner/work/pig-face-recognition/pig-face-recognition/build.properties'
+
+# a simple function to read an array of configuration files into a config object
+def read_config(cfg_files):
+    if(cfg_files != None):
+        config = configparser.RawConfigParser()
+
+        # merges all files into a single config
+        for i, cfg_file in enumerate(cfg_files):
+            if(os.path.exists(cfg_file)):
+                if(cfg_file == local_property_file):
+                    config.read(cfg_file)
+                    break
+                if(os.path.exists(cfg_file)):
+                    config.read(cfg_file)
+
+        return config
+
+#merge all into one config dictionary
+config = read_config([local_property_file, build_property_file])
 
 # Detection Config
 
@@ -14,10 +37,12 @@ output_path_cropped_rectangle = r"G:\temp\pig-face-22-03-2021"
 
 output_path_cropped_rectangle_test = r"G:\temp\pig-face-22-03-2021-test"
 
-image_example_name = r"DSC_V1_6460_2238.JPG"
+image_example_name = r"DSC_V1_6460_2238.jpg"
 
 max_image_number = 30
 
-image_upload_dir_path = r"D:\Users\avatar\PycharmProjects\pig-face-recognition\app\upload"
+# image_upload_dir_path = r"D:\Users\avatar\PycharmProjects\pig-face-recognition\app\upload"
+image_upload_dir_path = config.get('glob', 'image_upload_dir_path')
 
-output_dir_path = r"D:\Users\avatar\PycharmProjects\pig-face-recognition\output"
+# output_dir_path = r"D:\Users\avatar\PycharmProjects\pig-face-recognition\output"
+output_dir_path = config.get('glob', 'output_dir_path')
