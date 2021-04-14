2840eb329082fb555e3251ecf8dfb6ef8b111e81
PatrickRichner <patrick.richner@stud.hslu.ch>
2021-04-14 21:33:49 +0200

    Change documentation aug_util

--- augmentation/aug_util.py
+++ augmentation/aug_util.py
@@ -5,14 +5,14 @@ import numpy as np
 from PIL import Image as Pil_Image
 from matplotlib import pyplot as plt
 from albumentations import *
-
+from util import logger_init
 
 log = logging.getLogger(__name__)
 
 
 def save_aug_image(image_name, img_path, pig_img_aug1, prefix):
-    """Saves the augmented images"""
-    log.info('Saving augmented images...')
+    """Saves the augmented image"""
+    log.info('Saving augmented image...')
     img_aug_opencv = np.array(pig_img_aug1)
     pil_img = Pil_Image.fromarray(img_aug_opencv)
     aug_img_name = prefix + image_name
@@ -20,8 +20,8 @@ def save_aug_image(image_name, img_path, pig_img_aug1, prefix):
 
 
 def plot_image(pig_img_aug, label, rows):
-    """Plots the augmented images"""
-    log.info('Plotting augmented images...')
+    """Plots the augmented image"""
+    log.info('Plotting augmented image...')
     global num
     num = num + 1
     plt.subplot(rows, 5, num)
@@ -34,7 +34,7 @@ def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     Initialize the dimensions of the image to be resized and grab the
     image size.
     """
-    log.info('Resizing images...')
+    log.info('Resizing image...')
     dim = None
     (h, w) = image.shape[:2]
     # if both the width and height are None, then return the
@@ -64,12 +64,18 @@ def clean_augmented_images(path, pattern):
         - path : str Path of the to delete file in sub folders
         - pattern: str Pattern of the file beginning
     """
-    result = [y
-              for x in os.walk(path)
-              for y in glob.glob(os.path.join(x[0], pattern))
-              ]
-    for file in result:
+    log.info('Start deleting process of images with the pattern ' + pattern + '...')
+    all_files = [y
+                 for x in os.walk(path)
+                 for y in glob.glob(os.path.join(x[0], pattern))
+                 ]
+    if len(all_files) == 0:
+        log.warning('No files for this pattern ' + pattern + ' in the folders')
+    for file in all_files:
         os.remove(file)
+    log.info('Finished deleting process')
+
+
 
 
 
