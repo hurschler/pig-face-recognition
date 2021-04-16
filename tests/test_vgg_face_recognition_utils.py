789fd98e03fe813ace301a7be7be9367969e8904
PatrickRichner <patrick.richner@stud.hslu.ch>
2021-04-16 14:52:42 +0200

    Add documentation and remove unused imports

--- tests/test_vgg_face_recognition_utils.py
+++ tests/test_vgg_face_recognition_utils.py
@@ -1,12 +1,6 @@
-import os
-import tensorflow
 import util.config as config
-import numpy as np
-import jsonpickle
-import unittest
-
-from unittest import TestCase
 import recognition.vgg_face_recognition_utils_v1 as vgg_face_utils
+from unittest import TestCase
 from recognition.ml_data import MlData
 
 PATH_JSON_DATA = config.test_json
@@ -15,13 +9,13 @@ PATH_JSON_DATA = config.test_json
 class TestPreprocessing(TestCase):
 
     def setUp(self):
-        self.ml_data = MlData([],[],[],[], {})
+        self.ml_data = MlData([], [], [], [], {})
 
     def test_serialize_ml_data(self):
+        """Tests the serialisation of ml_Data"""
         ml_data = self.ml_data
         ml_data = vgg_face_utils.load_ml_data_from_json_file(
             ml_data,
             json_data=PATH_JSON_DATA
         )
         self.assertIsNotNone(ml_data, 'ML_Data is None')
-
